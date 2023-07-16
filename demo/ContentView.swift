5020c85f30488aa3e3b077ccf57a41fc425dc39c
Ge Will <531sunlight@gmail.com>
2023-07-16 12:46:10 +0800

    v1

--- demo/ContentView.swift
+++ demo/ContentView.swift
@@ -7,75 +7,82 @@
 
 import SwiftUI
 
-struct NewsObject:Decodable {
-    let data:Array<NewItem>
-    struct NewItem:Decodable {
-        let title : String;
-        let date : String;
-        let thumbnail_pic_s : String;
-    }
+struct NewsObject: Decodable {
+  let items: [NewItem]
+  struct NewItem: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let created_at: Double
+    let cover: String
+  }
 }
 
 struct ContentView: View {
-    @State private var newsData: Data?
-    @State private var newsArray:Array<NewsObject.NewItem>?
-    
+  @State private var newsData: Data?
+  @State private var newsArray: [NewsObject.NewItem]?
 
-    func getNewsData(){
-        let requireNewsUrl:URL = URL(string: "http://192.168.31.127:3000/news")!
-        let session = URLSession.shared
-        let request = URLRequest(url: requireNewsUrl)
-        
-        let task = session.dataTask(with: request as URLRequest){(data,response,error) in
-            if data != nil {
-                newsData = data
-            }
-        }
-        task.resume()
+  func getNewsData() {
+    let requireNewsUrl = URL(string: "https://opser.api.dgtle.com/v2/news/index")!
+    let session = URLSession.shared
+    let request = URLRequest(url: requireNewsUrl)
+
+    let task = session.dataTask(with: request as URLRequest) { data, _, _ in
+      if data != nil {
+        newsData = data
+      }
     }
-    
-    var body: some View {
-        List(){
-            if let newsArray = self.newsArray {
-                ForEach(newsArray, id: \.title) { newsItem in
-                    HStack {
-                        AsyncImage(url: URL(string: newsItem.thumbnail_pic_s),scale: 4)
-                            .frame(width: 60,height:20)
-                        VStack{
-                            Text(newsItem.title)
-                                .font(.system(size: 14))
-                                .padding(EdgeInsets(top: 0, leading:18, bottom:0, trailing:0))
-                                .lineLimit(1)
-                            Text(newsItem.date)
-                                .foregroundColor(.gray)
-                                .font(.system(size:12))
-                                .padding(EdgeInsets(top: 2, leading:0, bottom:0, trailing:0))
-                        }
-                    }.padding(10)
-                }
+    task.resume()
+  }
+
+  var body: some View {
+    List {
+      if let newsArray = self.newsArray {
+        ForEach(newsArray, id: \.id) { newsItem in
+          HStack(spacing: 10.0) {
+            AsyncImage(url: URL(string: newsItem.cover)) { image in
+              image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            } placeholder: {
+              ProgressView()
             }
-            else{
-                Text("网络请求错误")
+            .frame(width: 60, height: 60, alignment: .center)
+            .cornerRadius(10)
+
+            VStack(alignment: .leading, spacing: 10.0) {
+              Text(newsItem.title)
+                .font(.headline)
+                .lineLimit(2)
+              Text(Date(timeIntervalSince1970: newsItem.created_at).formatted())
+                .foregroundColor(.gray)
+                .font(.body)
             }
+          }
         }
-        .onAppear {
-            getNewsData()
-        }
-        .onChange(of:newsData){ newData in
-            if let news = try? JSONDecoder().decode(NewsObject.self, from: newData ?? Data()) {
-                DispatchQueue.main.async{
-                    newsArray = news.data
-                }
-            }
-            else {
-                print("JSON Parse Error!")
-            }
-        }
+      } else {
+        Text("网络请求错误")
+      }
     }
+    .refreshable {
+      getNewsData()
+    }
+    .onAppear {
+      getNewsData()
+    }
+    .onChange(of: newsData) { newData in
+      if let news = try? JSONDecoder().decode(NewsObject.self, from: newData ?? Data()) {
+        DispatchQueue.main.async {
+          newsArray = news.items
+        }
+      } else {
+        print("JSON Parse Error!")
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
